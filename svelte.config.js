3ebac79495efc3016e98f3c92ff620b91fd0c6e2
Vallrack <vallrack67@gmail.com>
2026-03-30 19:14:49 -0500

    Convertido load de server a universal para despliegue estático

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // <--- Cambia auto por static
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			// Esto es vital para que carguen las imágenes y estilos en GitHub Pages
+			base: process.env.NODE_ENV === 'production' ? '/prueba-actividad-2' : '',
+		}
 	}
 };
 
